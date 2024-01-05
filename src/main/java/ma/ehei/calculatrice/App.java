0aaa37004582aba73652d8972709456fa67ad20a
ghizlaneb2 <155394486+ghizlaneb2@users.noreply.github.com>
2024-01-05 16:53:37 +0100

    Update App.java

--- src/main/java/ma/ehei/calculatrice/App.java
+++ src/main/java/ma/ehei/calculatrice/App.java
@@ -2,6 +2,9 @@ package ma.ehei.calculatrice;
 
 public class App {
 	public static void main(String[] args) {
-		System.out.println(Calculatrice.add(5, 5));
-	}
+		
+ System.out.println("Résultat : " + Calculatrice.add(25, 25));
+        
+        // Appel à la fonction 
+        System.out.println("Résultat : " + Calculatrice.multiply(23, 32));	}
 }
